ddd8de07d9f9c61dc9a1e136e3b840e4be2b315c
Kostas Sitaras <kostassitaras1@gmail.com>
2026-02-18 19:55:40 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ export default function App() {
   src="gradient.png" alt="Grandient-img" />
 
   {/* Blur Effect Right */}
-  <div className="h-px w-[40rem] absolute top-[20%] right-[-5%] 
+  <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] 
   shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10" >
   </div>
   {/* Blur Effect Left */}
-  <div className="h-px w-[50rem] absolute top-[55%] left-[-10%] 
+  <div className="h-0 w-[50rem] absolute top-[55%] left-[-10%] 
   shadow-[0_0_900px_20px_#e99b63] -rotate-[-30deg] -z-10" >
   </div>
 
