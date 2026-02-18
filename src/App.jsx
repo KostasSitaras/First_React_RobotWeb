0e41b2c7988868a8dfa47e71d18aabd43b76f108
Kostas Sitaras <kostassitaras1@gmail.com>
2026-02-18 19:51:56 +0200

    Phone display update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
   })
   return (
  <main>
-  {/* Gradient Image */}
+  {/* Gradient Image Right */}
   <img className="absolute top-0 right-0 opacity-60 -z-10" 
   src="gradient.png" alt="Grandient-img" />
 
@@ -21,11 +21,11 @@ export default function App() {
   src="gradient.png" alt="Grandient-img" />
 
   {/* Blur Effect Right */}
-  <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] 
+  <div className="h-px w-[40rem] absolute top-[20%] right-[-5%] 
   shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10" >
   </div>
   {/* Blur Effect Left */}
-  <div className="h-0 w-[50rem] absolute top-[55%] left-[-10%] 
+  <div className="h-px w-[50rem] absolute top-[55%] left-[-10%] 
   shadow-[0_0_900px_20px_#e99b63] -rotate-[-30deg] -z-10" >
   </div>
 
