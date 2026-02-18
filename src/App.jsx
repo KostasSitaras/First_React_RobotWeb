0fa9d32ac53e7314989b93d6f3e9a16cc86392a0
Kostas Sitaras <kostassitaras1@gmail.com>
2026-02-18 18:27:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,8 @@ export default function App() {
   return (
  <main>
   {/* Gradient Image */}
-  <img className="absolute top-0 right-0 opacity-80 -z-10" 
-  src="/gradient.png" 
-  alt="Grandient-img" />
+  <img className="absolute top-0 right-0 opacity-80 -z-1" 
+  src="gradient.png" alt="Grandient-img" />
 
   {/* Blur Effect */}
   <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] 
