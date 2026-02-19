f9f51d45849b5f91162b93ff28fafec5cd5c3b1a
Kostas Sitaras <kostassitaras1@gmail.com>
2026-02-19 12:25:58 +0200

    Update on small and medium devices

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,16 @@ export default function App() {
   src="gradient.png" alt="Grandient-img" />
 
   {/* Gradient Image Left */}
-  <img className="absolute bottom-40 left-0 opacity-60 -z-10" 
+  <img className="absolute bottom-40 left-0 opacity-60 -z-10 hidden lg:block" 
   src="gradient.png" alt="Grandient-img" />
 
   {/* Blur Effect Right */}
   <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] 
-  shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10" >
+  shadow-[0_0_900px_40px_#e99b63] -rotate-[30deg] -z-10" >
   </div>
   {/* Blur Effect Left */}
   <div className="h-0 w-[50rem] absolute top-[55%] left-[-10%] 
-  shadow-[0_0_900px_20px_#e99b63] -rotate-[-30deg] -z-10" >
+  shadow-[0_0_900px_30px_#e99b63] -rotate-[-30deg] -z-10 hidden lg:block" >
   </div>
 
 
