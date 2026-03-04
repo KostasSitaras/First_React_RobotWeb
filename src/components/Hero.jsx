5be6d48147fc7ae9373eb1e18752044e16801a33
Kostas Sitaras <kostassitaras1@gmail.com>
2026-03-04 13:58:11 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,24 +6,8 @@ const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row 
     items-center justify-between min-h-[calc(90vh-6rem)]">
-        
-{/* This is the Home background Gradient And Blur Effect */}        
-{/* Gradient Image Right */}
-  <img className="absolute top-0 right-0 opacity-60 -z-10" 
-  src="gradient.png" alt="Grandient-img" />
 
-{/* Gradient Image Left */}
-  <img className="absolute bottom-40 left-0 opacity-60 -z-10 hidden lg:block" 
-  src="gradient.png" alt="Grandient-img" />
 
-{/* Blur Effect Right */}
-  <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] 
-  shadow-[0_0_900px_40px_#e99b63] -rotate-[30deg] -z-10" >
-  </div>
-{/* Blur Effect Left */}
-  <div className="h-0 w-[50rem] absolute top-[55%] left-[-10%] 
-  shadow-[0_0_900px_35px_#e99b63] -rotate-[-30deg] -z-10 hidden lg:block" >
-  </div>
         
         <div data-aos="fade-right"
             data-aos-offset="300"
