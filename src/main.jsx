838018e2230aa82b51e46dfa6a661258c1ab4701
Kostas Sitaras <kostassitaras1@gmail.com>
2026-03-04 14:07:55 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/First_React_RobotWeb/">
       <App />
     </BrowserRouter>
   </StrictMode>,
